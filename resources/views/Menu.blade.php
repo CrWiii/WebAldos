733b3d07a794b2ce816ea81f0641f31352011df1
Cristhian Sevilla <cgjsl89@hotmail.com>
2017-03-24 21:28:38 -0500

    gg

--- resources/views/Menu.blade.php
+++ resources/views/Menu.blade.php
@@ -13,15 +13,15 @@
         </div>
           <div id="navbar" class="navbar-collapse collapse" style="font-family: 'TrajanProBold' !important;font-size: 12px;">
               <ul class="nav navbar-nav navbar-left">
-                <li><a href="ElMundoAldoCo.html">EL MUNDO DE ALDO</a></li>
-                <li><a href="novios.html">NOVIOS</a></li>
-                <li><a href="joyeria.html">JOYERIA</a></li>
+                <li><a href="">EL MUNDO DE ALDO</a></li>
+                <li><a href="">NOVIOS</a></li>
+                <li><a href="">JOYERIA</a></li>
 
               </ul>
               <ul class="nav navbar-nav navbar-right" >
-                <li><a href="PatekPhilippe.html">PATEK PHILIPPE</a></li>
-                <li><a href="Eventos.html">EVENTOS</a></li>
-                <li><a href="Contactenos.html">CONTÁCTENOS</a></li>
+                <li><a href="">PATEK PHILIPPE</a></li>
+                <li><a href="">EVENTOS</a></li>
+                <li><a href="">CONTÁCTENOS</a></li>
               </ul>
             </div>
      </div>
