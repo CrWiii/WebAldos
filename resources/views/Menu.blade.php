bc43e59fe5ceda82a056ce835f1a4ed4d41b4f0c
Cristhian Sevilla <cgjsl89@hotmail.com>
2017-05-11 11:26:40 -0500

    gggg

--- resources/views/Menu.blade.php
+++ resources/views/Menu.blade.php
@@ -41,25 +41,24 @@
                       
                       <li class="dropdown dropdown-mega"><a href="{{url('Novios')}}">NOVIOS  <span class="caret"></span></a>
                         <div class="dropdown-menu dropdown-mega-content">
-                          <div class="row" style="padding: 20px; padding-bottom: 0">
-                            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12"></div>
-
+                          <div class="row" style="padding: 20px; padding-bottom: 0;text-align: center; margin:auto;width: 100%">
                             <style type="text/css">
-                      .asfa > img{
-                        max-width: 45% !important;
-                      }
-                    </style>
-                    @foreach($TypesNovios as $TypeN)
-                    @if($TypeN->slug!='colectivo_de_novios')
-                       <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-                              <ul>
-                              <span><img src="{{url('img/anillo.png')}}" alt="product 1" class="img-responsive img-hi"></span>
-                                <li style="text-align: center;"><a href="{{url('Novios',array('Type'=>$TypeN->slug))}}"><h3>{{$TypeN->description}}</h3></a></li>
-                              </ul>
+                              .asfa > img{
+                                max-width: 45% !important;
+                              }
+                            </style>
+                            <div class="col-lg-12"> 
+                            @foreach($TypesNovios as $TypeN)
+                              @if($TypeN->slug!='colectivo_de_novios')
+                                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                                  <ul>
+                                    <span><img src="{{url('img/anillo.png')}}" class="img-responsive img-hi"></span>
+                                      <li style="text-align: center;"><a href="{{url('Novios',array('Type'=>$TypeN->slug))}}">{{$TypeN->description}}</a></li>
+                                  </ul>
+                                </div>
+                              @endif
+                            @endforeach
                             </div>
-                    @endif
-                    @endforeach
-                                                        
                           </div>
                         </div>
                       </li>
@@ -67,27 +66,29 @@
                       <li class="dropdown dropdown-mega"><a href="{{url('Joyeria')}}">JOYERIA <span class="caret"></span></a>
                         <div class="dropdown-menu dropdown-mega-content">
                           <div class="row" style="padding: 20px; padding-bottom: 0">
-                            <div class="col-lg-10">
+                          <div class="col-lg-12"> 
+                            <!-- <div class="col-md-10"> -->
                             <?php $f=0;?>
                             @foreach($TypesJoyas as $TypeJ)
                               @if($f<6)
                               <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
                                 <ul>
-                                  <li style="text-align: center;"><a href="{{url('Joyeria',array('Type'=>$TypeJ->slug))}}"><span><img src="{{url('img/anillo.png')}}" class="img-responsive img-hi" alt="product 1" style="margin: auto;"><h3>{{$TypeJ->description}}</h3></a></li>
+                                  <li style="text-align: center;"><a href="{{url('Joyeria',array('Type'=>$TypeJ->slug))}}"><span><img src="{{url('img/anillo.png')}}" class="img-responsive img-hi" alt="product 1" style="margin: auto;">{{$TypeJ->description}}</a></li>
                                 </ul>
                               </div>
                               @endif
                               <?php $f++;?>
                             @endforeach
-                            </div>
-                            <div class="col-lg-2">
-                              <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
+                            <!-- </div> -->
+                            <!-- <div class="col-md-2">
+                              <div class="col-md-10 col-xs-2" style="padding-top: 3px;">
                                 <ul>
                                   <img src="{{url('img/anillo.png')}}" class="img-responsive" alt="product 1" style="margin: auto;" class="img-responsive img-hi">
                                   <li style="text-align: center;"><a href="{{url('Joyeria',array('Type'=>$TypeJ->description))}}"><h3>{{$TypeJ->description}}</h3></a></li>
                                 </ul>
                               </div>
-                            </div>
+                            </div> -->
+                          </div>
                           </div>
                         </div>
                       </li>
